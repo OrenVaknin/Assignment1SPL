26af49096b8b21b8ac5ff04304b40c5ff96000c3
Goldymen2002 <130919040+Goldymen2002@users.noreply.github.com>
2024-01-27 01:09:41 +0200

    Add files via upload

--- Skeleton/include/Order.h
+++ Skeleton/include/Order.h
@@ -6,8 +6,6 @@ using std::string;
 using std::vector;
 
 
-class Volunteer;
-
 enum class OrderStatus {
     PENDING,
     COLLECTING,
@@ -19,23 +17,23 @@ enum class OrderStatus {
 
 class Order {
 
-    public:
-        Order(int id, int customerId, int distance);
-        int getId() const;
-        int getCustomerId() const;
-        void setStatus(OrderStatus status);
-        void setCollectorId(int collectorId);
-        void setDriverId(int driverId);
-        int getCollectorId() const;
-        int getDriverId() const;
-        OrderStatus getStatus() const;
-        const string toString() const;
+public:
+    Order(int id, int customerId, int distance);
+    int getId() const;
+    int getCustomerId() const;
+    void setStatus(OrderStatus status);
+    void setCollectorId(int collectorId);
+    void setDriverId(int driverId);
+    int getCollectorId() const;
+    int getDriverId() const;
+    OrderStatus getStatus() const;
+    const string toString() const;
 
-    private:
-        const int id;
-        const int customerId;
-        const int distance;
-        OrderStatus status;
-        int collectorId; //Initialized to NO_VOLUNTEER if no collector has been assigned yet
-        int driverId; //Initialized to NO_VOLUNTEER if no driver has been assigned yet
+private:
+    const int id;
+    const int customerId;
+    const int distance;
+    OrderStatus status;
+    int collectorId; //Initialized to NO_VOLUNTEER if no collector has been assigned yet
+    int driverId; //Initialized to NO_VOLUNTEER if no driver has been assigned yet
 };
--- Skeleton/include/WareHouse.h
+++ Skeleton/include/WareHouse.h
@@ -1,40 +1,51 @@
 #pragma once
 #include <string>
 #include <vector>
-using namespace std;
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include "Order.h"
 #include "Customer.h"
+#include "Volunteer.h"
 
 class BaseAction;
-class Volunteer;
+
+using namespace std;
+
 
 // Warehouse responsible for Volunteers, Customers Actions, and Orders.
 
 
+
 class WareHouse {
 
-    public:
-        WareHouse(const string &configFilePath);
-        void start();
-        const vector<BaseAction*> &getActionsLog() const;
-        void addOrder(Order* order);
-        void addAction(BaseAction* action);
-        void printActionsLogs();
-        Customer &getCustomer(int customerId) const;
-        Volunteer &getVolunteer(int volunteerId) const;
-        Order &getOrder(int orderId) const;
-        void close();
-        void open();
+public:
+    
+    WareHouse(const string &configFilePath);
+    void start();
+    void addOrder(Order* order);
+    void addAction(BaseAction* action);
+    Customer& getCustomer(int customerId) const;
+    Volunteer& getVolunteer(int volunteerId) const;
+    Order& getOrder(int orderId) const;
+    const vector<BaseAction*>& getActions() const;
+    void close();
+    void open();
 
-    private:
-        bool isOpen;
-        vector<BaseAction*> actionsLog;
-        vector<Volunteer*> volunteers;
-        vector<Order*> pendingOrders;
-        vector<Order*> inProcessOrders;
-        vector<Order*> completedOrders;
-        vector<Customer*> customers;
-        int customerCounter; //For assigning unique customer IDs
-        int volunteerCounter; //For assigning unique volunteer IDs
+
+private:
+    bool isOpen;
+    vector<BaseAction*> actionsLog;
+    vector<Volunteer*> volunteers;
+    vector<Order*> pendingOrders;
+    vector<Order*> inProcessOrders;
+    vector<Order*> completedOrders;
+    vector<Customer*> customers;
+    int customerCounter; //For assigning unique customer IDs
+    int volunteerCounter; //For assigning unique volunteer IDs
+    int orderCounter;
+
 };
+
+
+
